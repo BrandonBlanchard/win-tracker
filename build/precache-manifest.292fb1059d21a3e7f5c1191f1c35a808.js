dc5cf5b5d5845c94d4adf9d8c2a3cac3b7f699e8
brandonblanchard <brandonb@DESKTOP-36N1M2V.localdomain>
2019-03-18 12:32:53 -0700

    Added build files for github pages

---
+++ build/precache-manifest.292fb1059d21a3e7f5c1191f1c35a808.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7cdd47f7294ac2e381e4",
+    "url": "/static/js/main.744d9004.chunk.js"
+  },
+  {
+    "revision": "f80a60f8d4e3f1c650dd",
+    "url": "/static/js/2.07131d3a.chunk.js"
+  },
+  {
+    "revision": "7cdd47f7294ac2e381e4",
+    "url": "/static/css/main.f1d33d7d.chunk.css"
+  },
+  {
+    "revision": "f80a60f8d4e3f1c650dd",
+    "url": "/static/css/2.d6032e97.chunk.css"
+  },
+  {
+    "revision": "e2eaf6f8679303ca904967ecefa47e60",
+    "url": "/index.html"
+  }
+];
