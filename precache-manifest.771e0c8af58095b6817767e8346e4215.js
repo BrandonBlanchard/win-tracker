8fc4c272154cb228f50f6e028f42c66cce296050
brandonblanchard <brandonb@DESKTOP-36N1M2V.localdomain>
2019-03-21 18:08:34 -0700

    Updates

---
+++ precache-manifest.771e0c8af58095b6817767e8346e4215.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/win-tracker/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8e0a512bc2e6e2f72d02",
+    "url": "/win-tracker/static/js/runtime~main.befbb89c.js"
+  },
+  {
+    "revision": "c1a44c22a0e13ebea602",
+    "url": "/win-tracker/static/js/main.118d7489.chunk.js"
+  },
+  {
+    "revision": "7124955533dfb7d871de",
+    "url": "/win-tracker/static/js/2.3fab6381.chunk.js"
+  },
+  {
+    "revision": "c1a44c22a0e13ebea602",
+    "url": "/win-tracker/static/css/main.c2d263e7.chunk.css"
+  },
+  {
+    "revision": "7124955533dfb7d871de",
+    "url": "/win-tracker/static/css/2.d6032e97.chunk.css"
+  },
+  {
+    "revision": "dc19b23d3f7378a1e120afa098d7c509",
+    "url": "/win-tracker/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/win-tracker/precache-manifest.e555efd2d2fda006145196d0154df973.js"
+  "/win-tracker/precache-manifest.771e0c8af58095b6817767e8346e4215.js"
 );
 
 workbox.clientsClaim();
